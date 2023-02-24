ac8d98e40ac790a9712529426eb6c5d78e3d6b2b
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-24 16:08:00 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+  },
+};

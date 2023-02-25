21b3886500beba2ece407539a51c124340eb1b87
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-25 20:54:57 +0100

    Test getUsers function

--- src/server/controllers/usersControllers/usersControllers.test.ts
+++ src/server/controllers/usersControllers/usersControllers.test.ts
@@ -1,7 +1,7 @@
 import { type Request, type Response } from "express";
 import User from "../../../database/models/User";
 import { type UserStructure } from "../../../types";
-import { createUser } from "./usersControllers";
+import { createUser, getUsers } from "./usersControllers";
 
 describe("Given a createUser function", () => {
   describe("When it receives a res object", () => {
@@ -66,5 +66,42 @@ describe("Given a createUser function", () => {
 
       expect(res.json).toHaveBeenCalledWith({ user });
     });
+  });
+});
+
+describe("Given a getUsers function", () => {
+  describe("When it receives a response object", () => {
+    const req = {} as Request;
+    const res = {status:jest.fn().mockReturnThis(), json:jest.fn()} as Partial<Response>;
+    const next = () => ({});
+
+    const users = {
+      image: "",
+      username: "",
+      password: "",
+      email: "",
+      enemies: {},
+      friends: {},
+    };
+
+    test("Then it should call the response status method with 200", async() => {
+      const statusCode = 200;
+
+      User.find = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValue(users),
+      }));
+      await getUsers(req, res as Response , next)
+
+      expect(res.status).toHaveBeenCalledWith(statusCode)
+    });
+
+    test("Then it should call the response json method with users that comes from stablish database", async() => {
+      User.find = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValue(users),
+      }));
+      await getUsers(req, res as Response, next);
+
+      expect(res.json).toHaveBeenCalledWith({users});
+    })
   });
 });

48b6029a9f64a34ffd3e50a2c6b9fc52249426bb
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-25 12:35:01 +0100

    Create a connectDatabase function to acces a database

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import "./loadEnvironments.js"
 import createDebug from "debug";
 import chalk from "chalk";
 import startServer from "./server/startServer.js";
+import connectDatabase from "./database/connectDatabase.js";
 
 const debug = createDebug("app");
 const port = process.env.PORT ?? 4500;
-
+const mongoDdUrl = process.env.MONGODB_CONNECTION_URL!;
 
 try{
   await startServer(+port)
   debug(chalk.bgGreen(`Start with server 'http://localhost:${port}'`));
-  
+
+  await connectDatabase(mongoDdUrl);
+  debug(chalk.bgGreen("Connected to data base"));
 }catch(error){
   debug(chalk.bgRed(error.message));
 }
